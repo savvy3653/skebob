902f316f02861ffc44445f6007d2bc66beb5a9e7
savvy3653 <savvyanimatics@gmail.com>
2025-08-18 23:48:33 +0300

    gitignore update

--- skebob/skebob.cpp
+++ skebob/skebob.cpp
@@ -1,7 +1,9 @@
-﻿#include <iostream>
+﻿#include "token.h"
+
+#include <iostream>
 #include <dpp/dpp.h>
 
 int main(int argc, char* argv[]) {
-    dpp::cluster bot("1200334234");
+    dpp::cluster bot(TOKEN);
     return 0;
 }
